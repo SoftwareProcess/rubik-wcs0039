dff1000b18889caf21ccd56470c5a9847cd61254
Clay <Clay@10.206.133.132>
2022-01-30 22:34:52 -0600

    Largely finished. Might add additional tests.

--- rubik/check.py
+++ rubik/check.py
@@ -1,10 +1,45 @@
-import rubik.cube as rubik
+#import rubik.cube as rubik
 
 def _check(parms):
     result={}
     encodedCube = parms.get('cube',None)       
     if(encodedCube == None):
-        result['status'] = 'error: xxx'
+        result['status'] = 'error: cube is missing'
     else:
         result['status'] = 'ok'
+    
+    if(isinstance(encodedCube, str) == False):
+        result['status'] = 'error: cube is not a string'
+        return result
+        
+    if(len(encodedCube) != 54):
+        result['status'] = 'error: cube is not the right length'
+        return result
+    
+    for char in encodedCube:
+        if(char.isalpha() == False and char.isdecimal() == False):
+            result['status'] = 'error: invalid character'
+            return result
+    
+    colors = [] #list to store characters
+    for char in encodedCube:
+        if(char not in colors):
+            colors.append(char)
+    if(len(colors) > 6):
+        result['status'] = 'error: too many colors'
+        return result
+    
+    if(len(colors) < 6):
+        result['status'] = 'error: too few colors'
+        return result
+    
+    for color in colors:
+        num = encodedCube.count(color)
+        if(num != 9):
+            result['status'] = 'error: colors are not equal'
+    
+    centers = [encodedCube[4], encodedCube[13], encodedCube[22], encodedCube[31], encodedCube[40], encodedCube[49]]
+    if(len(set(centers)) != len(centers)):
+        result['status'] = 'error: centers are not a unique color'
+    
     return result
--- rubik/test/checkTest.py
+++ rubik/test/checkTest.py
@@ -3,7 +3,7 @@ import rubik.check as check
 
 class CheckTest(TestCase):
         
-    def test_check_010_ShouldReturnOkOnSolvedCube(self):
+    def test_check_001_ShouldReturnOkOnSolvedCube(self):
         parm = {'op':'check',
                 'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
         result = check._check(parm)
@@ -11,3 +11,84 @@ class CheckTest(TestCase):
         status = result.get('status', None)
         self.assertEqual(status, 'ok')
 
+    def test_check_002_ShouldReturnErrorOnNonStringCube(self):
+        parm = {'op':'check',
+                'cube': 42}
+        result = check._check(parm)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube is not a string')
+        
+    def test_check_003_ShouldReturnErrorOnShortCube(self):
+        parm = {'op':'check',
+                'cube': 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwww'}
+        result = check._check(parm)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube is not the right length')
+    
+    def test_check_004_ShouldReturnErrorOnLongCube(self):
+        parm = {'op':'check',
+                'cube': 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwwww'}
+        result = check._check(parm)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube is not the right length')
+        
+    def test_check_005_ShouldReturnErrorOnReallyShortCube(self):
+        parm = {'op':'check',
+                'cube': 'b'}
+        result = check._check(parm)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube is not the right length')
+    
+    def test_check_006_ShouldReturnErrorOnReallyLongCube(self):
+        parm = {'op':'check',
+                'cube': 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'}
+        result = check._check(parm)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: cube is not the right length')
+        
+    def test_check_007_ShouldReturnErrorOnInvalidCharacter(self):
+        parm = {'op':'check',
+                'cube': '*********rrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: invalid character')
+        
+    def test_check_008_ShouldReturnErrorOnTooManyColors(self):
+        parm = {'op':'check',
+                'cube':'WWbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: too many colors')
+        
+    def test_check_009_ShouldReturnErrorOnTooFewColors(self):
+        parm = {'op':'check',
+                'cube':'rrrrrrrrrrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: too few colors')
+    
+    def test_check_010_ShouldReturnErrorOnUnequalColors(self):
+        parm = {'op':'check',
+                'cube':'wbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: colors are not equal')
+    
+    def test_check_011_ShouldReturnErrorOnDuplicateCenter(self):
+        parm = {'op':'check',
+                'cube':'bbrbrbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'error: centers are not a unique color')
+        
+    def test_check_012_ShouldReturnOkOnScrambledCube(self):
+        parm = {'op':'check',
+                'cube':'gybrbgggoobgowwwyboboggryorbbwwygwyyywyorrrbwrygroobwr'}
+        result = check._check(parm)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
