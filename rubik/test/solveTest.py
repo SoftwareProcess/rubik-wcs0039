927f4e1e15828056461a5d9e972cba22a83497d0
Clay <Clay@10.206.133.132>
2022-02-26 17:40:07 -0600

    Green Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -2,7 +2,14 @@ import unittest
 
 class SolveTest(unittest.TestCase):
 
-    def test_solve_H001_Should(self):
-        pass
+    def test_solve_H001_ShouldReturnOkOnValidCube(self):
+        parms = {'op':'check',
+                'cube':'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww',
+                'rotate': 'F'}
+        result = check._check(parms)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
+
 
     
