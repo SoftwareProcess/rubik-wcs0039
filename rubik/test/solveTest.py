b30ded4296fde669a48d1bea7e80932d36b2487e
Clay Shiver <wcs0039@auburn.edu>
2022-03-28 18:48:39 -0500

    Red Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -729,4 +729,22 @@ class SolveTest(unittest.TestCase):
         assertCube = rubik.Cube()
         assertCube.convertString(assertString)
         self.assertTrue(solve.checkFirstLayer(assertCube) == True)
+        
+    def test_SolveH048_ShouldSolveFrontRightCornerInUpLFOnMostlySolvedFirstLayer(self):
+        parms = {'op':'solve',
+                'cube':'ryrybgbboyryrrygrrobgggbgggyowoorooorybgybbobwwywwwwww'}
+        result = solve._solve(parms)
+        self.assertIn('status', result)
+        status = result.get('status', None)
+        self.assertEqual(status, 'ok')
+        solution = result.get('solution', None)
+        #putting provided solution into solve as 'rotate' to check if bottom cross is solved
+        assertParms = {'op':'solve',
+                       'cube': 'ryrybgbboyryrrygrrobgggbgggyowoorooorybgybbobwwywwwwww',
+                       'rotate': solution}
+        assertResult = solve._solve(assertParms)
+        assertString = assertResult.get('cube', None)
+        assertCube = rubik.Cube()
+        assertCube.convertString(assertString)
+        self.assertTrue(solve.checkFirstLayer(assertCube) == True)
                         
